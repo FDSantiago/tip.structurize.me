0b7bac12c18fea827c80451209a3e2d960b8c476
choke-dev <admin@choke.dev>
2024-09-19 09:35:08 +0000

    Initial Commit

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex items-center justify-center h-screen">
+    <span class="text-white font-mono">
+        {#if $page.status === 404}
+            <h1>Page not found</h1>
+            <p>Click <a href="/" class="underline">here</a> to return to the homepage.</p>
+        {:else}
+            <h1>An unexpected error occured.</h1>
+            <p>Error code: {$page.status}</p>
+        {/if}
+        
+    </span>
+</div>
+
+<!-- {#if status === 404}
+	<div class="flex items-center justify-center h-screen">
+		<span class="text-white">
+			<h1 class="font-mono text-center">404!</h1>
+			<p class="font-mono">The page you're looking for could not be found.</p>
+		</span>
+	</div>
+{:else}
+	<div class="flex items-center justify-center h-screen">
+		<span class="text-white">
+			<h1 class="font-mono text-center">Error {status}!</h1>
+			<p class="font-mono">{message}</p>
+		</span>
+	</div>
+{/if} -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center h-screen">
+    <span class="text-white font-mono">
+        <h1>Hi.</h1>
+        <p>This site is used to host activities, projects, assignments and more.</p>
+        <p>You can reach me out at <a class="underline" href="mailto:mfdsantiago@tip.edu.ph">mfdsantiago@tip.edu.ph</a></p>
+        <p>Or check out my <a class="underline" href="https://github.com/FDSantiago" target="_blank">github profile</a></p>
+    </span>
+</div>
